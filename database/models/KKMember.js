7168f27e8795247b723206d5941a3742c2f0ef11
Tahir Zahid <devtahirzahid@gmail.com>
2025-08-09 19:00:05 +0500

    added uniqueness on kk member endpoint

--- database/models/KKMember.js
+++ database/models/KKMember.js
@@ -13,6 +13,7 @@ const kkMemberSchema = new mongoose.Schema(
     contactNumber: {
       type: String,
       required: true,
+      unique: true,
     },
     residentialCity: {
       type: String,
